913e2ad3792164e817be656055c7b37e79386602
eric <collinseric321@gmail.com>
2024-12-07 12:47:06 -0800

    add write conditional

--- Wildfire/FWI/dask_ec2_test.py
+++ Wildfire/FWI/dask_ec2_test.py
@@ -243,7 +243,7 @@ def main(ec2_type: str):
     
     try:
         start_time = time.time()
-        if S3 and ds is not None:
+        if S3 and (ds is not None) and WRITE:
             fs = s3fs.S3FileSystem(anon=False)
             ds.to_zarr(
                 store=s3fs.S3Map(root=config.output_uri, s3=fs),
